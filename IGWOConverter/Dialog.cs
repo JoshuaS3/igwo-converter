55dd3d2b8291a82511ffca600f04589f4ed9ff0a
Joshua <joshstockin@gmail.com>
2018-02-15 16:35:34 -0600

    Form Creation

---
+++ IGWOConverter/Dialog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+namespace IGWOConverter
+{
+    public class Dialog
+    {
+        public Dialog()
+        {
+            Form converterDialog = new Form();
+            converterDialog.Text = "OBJ to Instant Gameworks Object (IGWO) Converter";
+            converterDialog.ShowDialog();
+        }
+    }
+}
