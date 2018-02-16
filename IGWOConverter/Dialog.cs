e624ca4508bf51ab5d1ca8ee419f317d0970aab9
Joshua <joshstockin@gmail.com>
2018-02-15 21:24:49 -0600

    Formatting + Clear Button

--- IGWOConverter/Dialog.cs
+++ IGWOConverter/Dialog.cs
@@ -24,8 +24,9 @@ namespace IGWOConverter
             converterDialog.Width = 680;
             converterDialog.Height = 490;
             converterDialog.BackColor = Color.FromArgb(245, 245, 245);
-            converterDialog.FormBorderStyle = FormBorderStyle.FixedSingle;
+            converterDialog.FormBorderStyle = FormBorderStyle.FixedDialog;
             converterDialog.MaximizeBox = false;
+            converterDialog.Icon = new Icon("InstantGameworksObject.ico");
             Label title = new Label();
             title.Text = "Instant Gameworks Object";
             title.Width = 640;
@@ -75,7 +76,7 @@ namespace IGWOConverter
             selectedSaveFile.Top = 130 + (25 - 12);
             selectedSaveFile.Parent = converterDialog;
             Button convertButton = new Button();
-            convertButton.Width = 620;
+            convertButton.Width = 300;
             convertButton.Height = 50;
             convertButton.Left = 20;
             convertButton.Top = 200;
@@ -84,6 +85,16 @@ namespace IGWOConverter
             convertButton.FlatStyle = FlatStyle.System;
             convertButton.Text = "CONVERT";
             convertButton.Parent = converterDialog;
+            Button clearButton = new Button();
+            clearButton.Width = 300;
+            clearButton.Height = 50;
+            clearButton.Left = 340;
+            clearButton.Top = 200;
+            clearButton.Font = new Font("Arial", 16f);
+            clearButton.BackColor = Color.LightGray;
+            clearButton.FlatStyle = FlatStyle.System;
+            clearButton.Text = "CLEAR";
+            clearButton.Parent = converterDialog;
             TextBox outputLog = new TextBox();
             outputLog.ReadOnly = true;
             outputLog.Multiline = true;
@@ -92,10 +103,22 @@ namespace IGWOConverter
             outputLog.BackColor = Color.White;
             outputLog.Width = 620;
             outputLog.Height = 160;
-            outputLog.Text = "";
+            outputLog.Font = new Font("Arial", 11f);
+            outputLog.Text = "Select a file and output to begin conversion";
             outputLog.Left = 20;
             outputLog.Top = 270;
             outputLog.Parent = converterDialog;
+            clearButton.Click += (object sender, EventArgs e) =>
+            {
+                openPath = null;
+                savePath = null;
+                selectedFile.Text = "<input file>";
+                selectedSaveFile.Text = "<output file>";
+                outputLog.Text = "Select a file and output to begin conversion";
+                selectedFile.Update();
+                selectedSaveFile.Update();
+                outputLog.Update();
+            };
             openFileButton.Click += (object sender, EventArgs e) =>
             {
                 OpenFileDialog newOpenFileDialog = new OpenFileDialog();
@@ -123,12 +146,23 @@ namespace IGWOConverter
             };
             convertButton.Click += (object sender, EventArgs e) =>
             {
+                if (openPath == null | savePath == null)
+                {
+                    outputLog.AppendText(Environment.NewLine);
+                    outputLog.AppendText("Please select file input and output");
+                    return;
+                }
+                outputLog.AppendText(Environment.NewLine);
                 outputLog.AppendText("-----------------------------------------");
+                outputLog.AppendText(Environment.NewLine);
                 outputLog.AppendText("Input: " + openPath);
+                outputLog.AppendText(Environment.NewLine);
                 outputLog.AppendText("Output: " + savePath);
                 outputLog.AppendText(Environment.NewLine);
                 outputLog.AppendText(Environment.NewLine);
                 outputLog.AppendText("Beginning conversion process");
+                outputLog.AppendText(Environment.NewLine);
+                outputLog.AppendText(Environment.NewLine);
 
                 Stopwatch timer = new Stopwatch();
                 timer.Start();
@@ -137,9 +171,13 @@ namespace IGWOConverter
 
                 outputLog.AppendText(Environment.NewLine);
                 outputLog.AppendText(output.Positions.Length + "\tvertices");
+                outputLog.AppendText(Environment.NewLine);
                 outputLog.AppendText(output.TextureCoordinates.Length + "\ttexture coordinates");
+                outputLog.AppendText(Environment.NewLine);
                 outputLog.AppendText(output.Normals.Length + "\tnormals");
+                outputLog.AppendText(Environment.NewLine);
                 outputLog.AppendText(output.Faces.Length + "\tfaces");
+                outputLog.AppendText(Environment.NewLine);
                 outputLog.AppendText(Environment.NewLine);
                 outputLog.AppendText(Environment.NewLine);
 
@@ -149,15 +187,19 @@ namespace IGWOConverter
                 long outputSize = new FileInfo(savePath).Length;
 
                 outputLog.AppendText("Input file size:  " + inputSize / 1024f + " KB\t(" + inputSize / 1048576f + " MB)");
+                outputLog.AppendText(Environment.NewLine);
                 outputLog.AppendText("Output file size: " + outputSize / 1024f + " KB\t(" + outputSize / 1048576f + " MB)");
                 outputLog.AppendText(Environment.NewLine);
+                outputLog.AppendText(Environment.NewLine);
                 outputLog.AppendText("File conversion efficiency: " + ((float)inputSize / (float)outputSize) * 100f + "% (output is " + ((float)outputSize / (float)inputSize) * 100f + "% the size of the original)");
+                outputLog.AppendText(Environment.NewLine);
                 outputLog.AppendText(Environment.NewLine);
                 outputLog.AppendText(Environment.NewLine);
 
                 timer.Stop();
 
                 outputLog.AppendText("Finished (" + timer.ElapsedTicks / 1000000f + " seconds)");
+                outputLog.AppendText(Environment.NewLine);
                 outputLog.AppendText("Success!");
             };
 
